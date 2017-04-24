24933489ce5fcab36928e708d60f01c09c1cd5ee
danyalsiraj <danyal61@gmail.com>
2017-04-24 14:05:50 -0400

    1

---
+++ char_boolean/src/char_boolean.java
@@ -0,0 +1,18 @@
+public class char_boolean
+{
+	public static void main (String[] args)
+	{
+		// unicode character 
+		char copyright_symbol='\u00Ae';
+		System.out.println(copyright_symbol);
+	
+		// boolean is just true and false 
+		// can be assigned with if statements to check if the condition is true or false
+		
+		boolean istrue= true;
+		boolean isfalse = false;
+		
+		
+	
+	}
+}
---
+++ strings/src/strings.java
@@ -0,0 +1,23 @@
+//byte
+//short
+//int
+//long
+//float
+//double
+//char
+//boolean
+
+
+public class strings{
+	
+	public static void main (String[] args)
+	{
+		String mystring="whats good niga";
+		System.out.println(mystring);
+		
+		//unicode characters in the string 
+		mystring="\u00A9 2017"; //should print copyright 2017
+		System.out.println(mystring);
+	}
+	
+}

8f56073b1c9f39f6f43ee78ce915ba06e5e062cd
danyalsiraj <danyal61@gmail.com>
2017-10-02 20:53:09 -0400

    added test for remove

--- examples/src/Test/utils/MyTest.java
+++ examples/src/Test/utils/MyTest.java
@@ -13,8 +13,25 @@ public class MyTest {
 	public void addToArray() {
 		Array<Person> tester = new Array<Person>();
 		assertTrue("person can not be added", tester.add(new Person("da", "ss")));
-		assertEquals("size should be 1",1,tester.size());
-		assertEquals("first element does not equal expected",new Person("da", "ss") ,tester.get(0));
+		assertEquals("size should be 1", 1, tester.size());
+		assertEquals("first element does not equal expected", new Person("da", "ss"), tester.get(0));
+	}
+
+	@Test
+	public void removeFromArrray() {
+		Array<Person> tester = new Array<Person>();
+		assertTrue("person can not be added", tester.add(new Person("da", "ss")));
+		assertTrue("person can not be removed", tester.remove(tester.get(0)));// remove
+																				// by
+																				// object
+		assertEquals("size should be 0", 0, tester.size());
+		assertTrue("person can not be added", tester.add(new Person("da", "ss")));
+		assertEquals("size should be 1", 1, tester.size());
+		assertEquals("person can not be removed", new Person("da", "ss"), tester.remove(0));// remove
+																							// by
+																							// index
+		assertEquals("size should be 0", 0, tester.size());
+
 	}
 
 }

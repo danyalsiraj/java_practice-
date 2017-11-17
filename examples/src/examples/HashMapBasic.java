f6e8356dbb41ed81f4aeac04e65c392760e95dab
danyalsiraj <danyal61@gmail.com>
2017-11-16 22:03:49 -0500

    HashMap example on hackerrank- https://www.hackerrank.com/challenges/phone-book/problem

---
+++ examples/src/examples/HashMapBasic.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+class HashMapBasic{
+   public static void main(String []argh)
+   {
+      HashMap<String, Integer> hmap= new HashMap<String,Integer>();
+      Scanner in = new Scanner(System.in);
+      int n=in.nextInt();
+      in.nextLine();
+      for(int i=0;i<n;i++)
+      {
+         String name=in.nextLine();
+         int phone=in.nextInt();
+          hmap.put(name,phone);
+         in.nextLine();
+      }
+      while(in.hasNext())
+      {
+         String s=in.nextLine();
+          if (hmap.containsKey(s)){
+              System.out.printf("%s=%d \n",s,hmap.get(s));
+          }else{
+              System.out.println("Not found");
+          }
+      }
+   }
+}
